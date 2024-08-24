03bdafb6766bf37c2ac83da86ee08c9672450868
robinrdj <robinrajaduraij@gmail.com>
2024-08-24 23:03:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isVisible,setIsVisible] = useState(false);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setIsVisible(true);
+  }
+  function handleFirstNameChange(e){
+     setFirstName(e.target.value);
+     setIsVisible(false);
+  }
+  function handleLastNameChange(e){
+    setLastName(e.target.value);
+    setIsVisible(false);
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h3>Full Name Display</h3>
+       <form onSubmit={handleSubmit}>
+        <div>
+        <label>First Name:</label>
+        <input type="text" onChange={handleFirstNameChange} required/>
+        </div>
+        <div>
+        <label>Last Name:</label>
+        <input type="text" onChange={handleLastNameChange} required/>
+        </div>
+       <button type='submit'>Submit</button>
+       </form>
+
+       <p>{isVisible &&  `Name:${firstName} ${lastName}`}</p>
     </div>
   );
 }
